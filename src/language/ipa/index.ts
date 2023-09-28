c1e7bda9c9574b7839b173744c0d0ff8f525fd67
Ben Coveney <bencoveney@gmail.com>
2023-09-29 00:18:36 +0100

    Move language utils

--- src/language/ipa/index.ts
+++ src/language/ipa/index.ts
@@ -13,6 +13,7 @@ import {
   getPossibleSyllableStructures,
   stringifySyllableStructure,
 } from "./phonotactics";
+import { findValues } from "./utils";
 
 export function validate() {
   console.log("consonants", findValues(consonants, englishConsonants));
@@ -31,40 +32,4 @@ export function validate() {
       (syllableStructure) => stringifySyllableStructure(syllableStructure)
     )
   );
-}
-
-function findValues(
-  set: { ipaCharacter: string }[],
-  find: string[]
-): { found: string[]; missing: string[] } {
-  const found: string[] = [];
-  const missing: string[] = [];
-  for (let index = 0; index < find.length; index++) {
-    const toFind = stripDiacritics(find[index]);
-    if (toFind.length > 1) {
-      const parts = toFind.split("");
-      const match = parts.every((partToFind) =>
-        set.find((potential) => potential.ipaCharacter === partToFind)
-      );
-      if (match) {
-        found.push(toFind);
-      } else {
-        missing.push(toFind);
-      }
-    } else {
-      const match = set.find((potential) => potential.ipaCharacter === toFind);
-      if (match) {
-        found.push(toFind);
-      } else {
-        missing.push(toFind);
-      }
-    }
-  }
-  return { found, missing };
-}
-
-// https://en.wikipedia.org/wiki/International_Phonetic_Alphabet#Diacritics_and_prosodic_notation
-export function stripDiacritics(value: string): string {
-  // Lol
-  return value.replaceAll("ː", "");
 }
