78b8b8834338b8ef0e18b03fed245793df5675c4
Ben Coveney <bencoveney@gmail.com>
2023-12-30 13:04:51 +0000

    Sample sentences from logs

---
+++ src/language/samples/sentencesFromLogs.ts
@@ -0,0 +1,45 @@
+export const sentencesFromLogs: string[] = [
+  // Init
+  "[being] woke from their slumber",
+  // Decision
+  "[being] set out for an unknown land",
+  "[being] chose to adopt a symbol in [location]",
+  "[being] chose to rest in [location]",
+  "[being] chose to explore in [location]", // discover
+  "[being] chose to travel in [location]",
+  "[being] chose to create architecture in [location]",
+  "[being] chose to join the creation of architecture in [location]",
+  "[being] chose to create an artifact in [location]",
+  "[being] chose to join the creation of an artifact in [location]",
+  "[being] chose to give an artifact in [location]",
+  "[being] chose to have a conversation in [location]",
+  "[being] chose to join the conversation in [location]",
+  // Movement
+  "[being] discovered the region of [location]",
+  "[being] entered the world in [location]",
+  "[being] moved from [location] to [location]",
+  "[being] completed their journey",
+  // Symbol adoption
+  "[being] started adopting a symbol",
+  "[being] adopted the [symbol] as their symbol",
+  // Rest
+  "[being] rested in [location]",
+  "[being] finished resting in [location]",
+  // Architecture creation
+  "[being] started creating architecture in [location]",
+  "[being] created [architecture] in [location]",
+  "[being] and [being] created [architecture] in [location]",
+  "[being], [being] and [being] created [architecture] in [location]",
+  // Conversation
+  "[being] and [being] started a conversation in [location]",
+  "[being] and [being] finished their conversation in [location]",
+  "[being], [being] and [being] finished their conversation in [location]",
+  // Artifact creation
+  "[being] started forging an artifact in [location]",
+  "[being] created the [object] [artifact] in [location]",
+  "[being] and [being] created the [object] [artifact] in [location]",
+  "[being], [being] and [being] created the [object] [artifact] in [location]",
+  // Artifact giving
+  "[being] started gifing an artifact to [being]",
+  "[being] gifted the [object] [artifact] to [being]",
+];
