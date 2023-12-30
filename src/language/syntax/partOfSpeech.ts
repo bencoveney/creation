18fcdb9e3c7205d7c04d974e42933fc080b6a50f
Ben Coveney <bencoveney@gmail.com>
2023-12-30 21:32:30 +0000

    Define some parts of speech

---
+++ src/language/syntax/partOfSpeech.ts
@@ -0,0 +1,46 @@
+enum PartOfSpeech {
+  /**
+   * The name of a specific object or set of objects, such as living creatures, places, actions, qualities, states of existence, or ideas.
+   */
+  Noun,
+  /**
+   * Conveys an action, an occurrence, or a state of being.
+   */
+  Verb,
+  /**
+   * Describes or defines a noun or noun phrase.
+   */
+  Adjective,
+  /**
+   * Modifies a verb, adjective, another adverb, determiner, clause, preposition, or sentence.
+   */
+  Adverb,
+  /**
+   * May substitute for a noun or noun phrase.
+   */
+  Pronoun,
+  /**
+   * Express spatial or temporal relations or mark various semantic roles.
+   */
+  Preposition,
+  /**
+   * Connects words, phrases, or clauses that are called the conjuncts of the conjunctions.
+   */
+  Conjunction,
+  /**
+   * Occurs as an utterance on its own and expresses a spontaneous feeling or reaction.
+   */
+  Interjection,
+  /**
+   * Describes a numerical quantity.
+   */
+  Numeral,
+  /**
+   * Mark the identifiability of the referents of the noun phrases.
+   */
+  Article,
+  /**
+   * Occurs together with a noun or noun phrase and generally serves to express the reference of that noun or noun phrase in the context.
+   */
+  Determiner,
+}
