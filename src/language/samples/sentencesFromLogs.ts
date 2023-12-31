3a2a71a6ec09b09d76e89f3e7716e5ff7f9c1576
Ben Coveney <bencoveney@gmail.com>
2023-12-31 17:30:53 +0000

    Annotate some decision sentences

--- src/language/samples/sentencesFromLogs.ts
+++ src/language/samples/sentencesFromLogs.ts
@@ -1,19 +1,187 @@
 export const sentencesFromLogs: string[] = [
   // Init
+
+  /**
+   * ???
+   *
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - woke
+   * Object:
+   * - Noun: slumber
+   *   - Possessive determiner: their
+   *
+   * "from" - preposition?
+   */
   "[being] woke from their slumber",
+
   // Decision
+
+  /**
+   * ???
+   *
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - set
+   * Object:
+   * - Noun: land
+   *   - Indefinite article: an
+   *   - Adjective: unknown
+   *
+   * "out for"
+   */
   "[being] set out for an unknown land",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to adopt
+   * - Argument:
+   *   - Noun: symbol
+   *     - Indefinite article: a
+   */
   "[being] chose to adopt a symbol in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to rest
+   */
   "[being] chose to rest in [location]",
-  "[being] chose to explore in [location]", // discover
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to explore
+   */
+  "[being] chose to explore in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to travel
+   */
   "[being] chose to travel in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to create
+   * - Argument:
+   *   - Noun: architecture
+   */
   "[being] chose to create architecture in [location]",
+
+  /**
+   * ???
+   *
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to join
+   * - Argument:
+   *
+   * "the creation of architecture"
+   */
   "[being] chose to join the creation of architecture in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to create
+   * - Argument:
+   *   - Noun: artifact
+   *     - Indefinite article: an
+   */
   "[being] chose to create an artifact in [location]",
+
+  /**
+   * ???
+   *
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to join
+   * - Argument:
+   *
+   * "the creation of an artifact"
+   */
   "[being] chose to join the creation of an artifact in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to give
+   * - Argument:
+   *   - Noun: artifact
+   *     - Indefinite article: an
+   */
   "[being] chose to give an artifact in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to have
+   * - Argument:
+   *   - Noun: conversation
+   *     - Indefinite article: a
+   */
   "[being] chose to have a conversation in [location]",
+
+  /**
+   * Subject:
+   * - Proper noun: [being]
+   * Verb:
+   * - chose
+   *   - Adverb: in [location]
+   * Object:
+   * - Infinitive: to join
+   * - Argument:
+   *   - Noun: conversation
+   *     - Definite article: the
+   */
   "[being] chose to join the conversation in [location]",
+
   // Movement
   "[being] discovered the region of [location]",
   "[being] entered the world in [location]",
